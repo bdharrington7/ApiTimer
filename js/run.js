48c74c478032d72e1243ce8ab10bcaf2ffbd8a88
bdharrington7 <BDHarrington7@gmail.com>
2015-02-03 18:14:45 -0800

    check for CORS in browser

--- js/run.js
+++ js/run.js
@@ -31,6 +31,30 @@ function validateEndpoints(endpoint1, endpoint2) {
   return true;
 }
 
+function createCORSRequest(method, url) {
+  var xhr = new XMLHttpRequest();
+  if ("withCredentials" in xhr) {
+
+    // Check if the XMLHttpRequest object has a "withCredentials" property.
+    // "withCredentials" only exists on XMLHTTPRequest2 objects.
+    xhr.open(method, url, true);
+
+  } else if (typeof XDomainRequest != "undefined") {
+
+    // Otherwise, check if XDomainRequest.
+    // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+
+  } else {
+
+    // Otherwise, CORS is not supported by the browser.
+    xhr = null;
+
+  }
+  return xhr;
+}
+
 function buttonGo() {
   // check entries in both api boxes
   var endpoint1 = $('#endpointA').val();
@@ -39,10 +63,13 @@ function buttonGo() {
     return;
   }
 
-  // create XHR request
-  var reqA = new XMLHttpRequest();
-  reqA.open('GET', endpoint1, true);
+  // create XHR request for A
+  var reqA = createCORSRequest($('#httpMethodA').val(), endpoint1);
+  if (!reqA) {
+    throw new Error('CORS not supported');
+  }
   reqA.onload = function (e) {
+    console.log(reqA.responseText);
     window.performance.mark('mark_endA_xhr');
     window.performance.measure('measure_xhrA', 'mark_startA_xhr', 'mark_endA_xhr');
     console.log('returned', e);
